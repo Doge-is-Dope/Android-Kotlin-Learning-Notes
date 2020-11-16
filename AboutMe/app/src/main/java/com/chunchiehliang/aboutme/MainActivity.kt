79f846fb969ab1f01dd80fe457b4cd7c928c921d
doge-is-dope <maple3312936@outlook.com>
2020-11-16 22:21:46 +0800

    feat: implement data binding

--- AboutMe/app/src/main/java/com/chunchiehliang/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/chunchiehliang/aboutme/MainActivity.kt
@@ -15,10 +15,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    // Instance of MyName data class.
+    private val myName: MyName = MyName("Aleks Haecky")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // Set the value of the myName variable that is declared and used in the layout file.
+        binding.myName = myName
+
+        // Click listener for the Done button.
         binding.doneButton.setOnClickListener {
             addNickname(it)
         }
@@ -26,7 +33,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = binding.nicknameEdit.text
+            // Set the text for nicknameText to the value in nicknameEdit.
+            myName?.nickname = nicknameEdit.text.toString()
             // Invalidates all binding expressions and requests a new rebind to refresh UI.
             invalidateAll()
             nicknameEdit.visibility = View.GONE
@@ -34,7 +42,6 @@ class MainActivity : AppCompatActivity() {
             nicknameText.visibility = View.VISIBLE
 
         }
-
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
