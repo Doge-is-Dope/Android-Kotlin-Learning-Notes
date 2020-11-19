08ff756577f703b7566545d6f3198a8900bf03bb
doge-is-dope <maple3312936@outlook.com>
2020-11-19 22:47:23 +0800

    feat: replace navigation to action IDs with NavDirections

--- Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -18,7 +18,7 @@ class TitleFragment : Fragment() {
                 inflater, R.layout.fragment_title, container, false)
 
         binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(
-                R.id.action_titleFragment_to_gameFragment)
+                TitleFragmentDirections.actionTitleFragmentToGameFragment())
         )
         setHasOptionsMenu(true)
         return binding.root
