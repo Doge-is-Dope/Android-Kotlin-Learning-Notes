16454ae0f580f9f930e6c4403a2a42e0a0b3d184
doge-is-dope <maple3312936@outlook.com>
2020-11-24 22:24:13 +0800

    feat: use lifecycle lib for timer

--- DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
 
-        dessertTimer = DessertTimer()
+        dessertTimer = DessertTimer(this.lifecycle)
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -91,7 +91,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStart() {
         super.onStart()
         Timber.i("onStart called")
-        dessertTimer.startTimer()
     }
 
     override fun onResume() {
@@ -108,7 +107,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     override fun onStop() {
         super.onStop()
         Timber.i("onStop")
-        dessertTimer.stopTimer()
     }
 
     override fun onDestroy() {
