c8c90ba36308c479ab118a72466ff1e2a3ffa14a
doge-is-dope <maple3312936@outlook.com>
2020-11-30 22:49:12 +0800

    feat: add LiveData encapsulation

--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -24,7 +24,6 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
@@ -40,7 +39,7 @@ class GameFragment : Fragment() {
     private lateinit var binding: GameFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
 
         // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
