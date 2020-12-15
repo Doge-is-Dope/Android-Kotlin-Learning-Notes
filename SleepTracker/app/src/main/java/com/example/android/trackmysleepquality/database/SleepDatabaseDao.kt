3fc62f9d85aaaa7feefcb2b53f247b219dd62320
doge-is-dope <maple3312936@outlook.com>
2020-12-15 23:11:17 +0800

    feat: create SleepDatabaseDao

--- SleepTracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ SleepTracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,54 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SleepDatabaseDao {
+    @Insert
+    suspend fun insertNight(night: SleepNight)
+
+    /**
+     * When updating a row with a value already set in a column,
+     * replaces the old value with the new one.
+     *
+     * @param night new value to write
+     */
+    @Update
+    suspend fun updateNight(night: SleepNight)
+
+    /**
+     * Selects and returns the row that matches the supplied start time, which is our key.
+     *
+     * @param key startTimeMilli to match
+     */
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    suspend fun getNight(key: Long): SleepNight?
+
+    @Delete
+    suspend fun deleteAllNights(nights: List<SleepNight>): Int
+
+    /**
+     * Deletes all values from the table.
+     *
+     * This does not delete the table, only its contents.
+     */
+    @Query("DELETE FROM daily_sleep_quality_table")
+    suspend fun clear()
+
+    /**
+     * Selects and returns all rows in the table,
+     *
+     * sorted by start time in descending order.
+     */
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    suspend fun getAllNights(): LiveData<List<SleepNight>>
+
+    /**
+     * Selects and returns the latest night.
+     */
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    suspend fun getTonight(): SleepNight?
+}
+
