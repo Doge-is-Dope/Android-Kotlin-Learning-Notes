ae3cce0740bc8f8c25c29f3f2b29f5954ca4b93d
doge-is-dope <maple3312936@outlook.com>
2021-02-10 21:49:56 +0800

    feat: set the content view as the canvas

--- 13_MiniPaint/app/src/main/java/com/chunchiehliang/minipaint/MainActivity.kt
+++ 13_MiniPaint/app/src/main/java/com/chunchiehliang/minipaint/MainActivity.kt
@@ -2,10 +2,21 @@ package com.chunchiehliang.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
+
+        setContentView(myCanvasView)
     }
 }
