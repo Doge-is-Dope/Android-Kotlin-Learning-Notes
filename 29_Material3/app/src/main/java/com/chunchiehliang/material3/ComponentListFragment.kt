51ab262dd7d8b6c31f7ed35df26358fad73fffe7
doge-is-dope <maple3312936@outlook.com>
2021-11-25 21:33:08 +0800

    chore: add layouts

--- 29_Material3/app/src/main/java/com/chunchiehliang/material3/ComponentListFragment.kt
+++ 29_Material3/app/src/main/java/com/chunchiehliang/material3/ComponentListFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.chunchiehliang.material3.databinding.FragmentComponentListBinding
 
 
 class ComponentListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_component_list, container, false)
+    ): View {
+        val binding = FragmentComponentListBinding.inflate(layoutInflater)
+
+
+
+        return binding.root
     }
 }
---
+++ 29_Material3/app/src/main/java/com/chunchiehliang/material3/EpoxyDataBindingPatterns.kt
@@ -0,0 +1,8 @@
+package com.chunchiehliang.material3
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern
+
+@EpoxyDataBindingPattern(rClass = R::class, layoutPrefix = "item")
+object EpoxyDataBindingPatterns
+
+
