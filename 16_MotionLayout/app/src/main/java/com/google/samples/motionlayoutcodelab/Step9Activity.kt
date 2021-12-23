cd6d08c947a5e22a736176082cfca8c01cb89d1f
clement.l <maple3312936@outlook.com>
2021-12-23 18:07:31 +0800

    feat: listen to animation completion

--- 16_MotionLayout/app/src/main/java/com/google/samples/motionlayoutcodelab/Step9Activity.kt
+++ 16_MotionLayout/app/src/main/java/com/google/samples/motionlayoutcodelab/Step9Activity.kt
@@ -1,14 +1,46 @@
 package com.google.samples.motionlayoutcodelab
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.appbar.AppBarLayout
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class Step9Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step9)
+
+        val transitionListener = object : MotionLayout.TransitionListener {
+
+            override fun onTransitionStarted(p0: MotionLayout?, startId: Int, endId: Int) {}
+
+            override fun onTransitionChange(
+                layout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, currentId: Int) {
+                if (currentId == R.id.set4)
+                    Log.d("Step9", "finished set4 $currentId")
+                else
+                    Log.d("Step9", "finished: $currentId")
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float
+            ) {
+            }
+        }
+
+        findViewById<MotionLayout>(R.id.motion_layout).setTransitionListener(transitionListener)
     }
 }
