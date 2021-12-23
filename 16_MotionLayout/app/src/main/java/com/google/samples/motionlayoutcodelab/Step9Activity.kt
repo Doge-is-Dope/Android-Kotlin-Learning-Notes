2411cfb9a631d337f393b37b2a8945c750584a28
clement.l <maple3312936@outlook.com>
2021-12-23 17:20:17 +0800

    feat: add animation in sequence

---
+++ 16_MotionLayout/app/src/main/java/com/google/samples/motionlayoutcodelab/Step9Activity.kt
@@ -0,0 +1,14 @@
+package com.google.samples.motionlayoutcodelab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
+
+class Step9Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step9)
+    }
+}
