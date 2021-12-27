750e1ce309aff1a8d7fb08a35cc35dc75f01ad57
clement.l <maple3312936@outlook.com>
2021-12-27 12:19:32 +0800

    feat: add deep link

--- 27_NavigationSample/app/src/main/java/com/chunchiehliang/navigationsample/explore/ExploreFragment.kt
+++ 27_NavigationSample/app/src/main/java/com/chunchiehliang/navigationsample/explore/ExploreFragment.kt
@@ -6,15 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.chunchiehliang.navigationsample.R
 import com.chunchiehliang.navigationsample.databinding.FragmentExploreBinding
+import timber.log.Timber
 
 class ExploreFragment : Fragment() {
 
     private var _binding: FragmentExploreBinding? = null
     private val binding get() = _binding!!
+
+    private val args by navArgs<ExploreFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +38,8 @@ class ExploreFragment : Fragment() {
             )
             toolbar.setupWithNavController(findNavController(), appBarConfiguration)
         }
+
+        Timber.d("args postNo: ${args.postNo}")
     }
 
     override fun onDestroyView() {
