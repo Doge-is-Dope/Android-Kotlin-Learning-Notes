f13cd786221e039db37380517604ab4b097a57e3
clement.l <maple3312936@outlook.com>
2021-12-29 18:56:59 +0800

    feat: add object animation

--- 16_MotionLayout/app/src/main/java/com/google/samples/motionlayoutcodelab/Step9Activity.kt
+++ 16_MotionLayout/app/src/main/java/com/google/samples/motionlayoutcodelab/Step9Activity.kt
@@ -1,10 +1,19 @@
 package com.google.samples.motionlayoutcodelab
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.animation.addListener
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.coroutines.delay
@@ -15,7 +24,8 @@ class Step9Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step9)
-
+        val btnFade = findViewById<Button>(R.id.btn_fade)
+        val mask = findViewById<View>(R.id.view_mask)
         val transitionListener = object : MotionLayout.TransitionListener {
 
             override fun onTransitionStarted(p0: MotionLayout?, startId: Int, endId: Int) {}
@@ -42,5 +52,20 @@ class Step9Activity : AppCompatActivity() {
         }
 
         findViewById<MotionLayout>(R.id.motion_layout).setTransitionListener(transitionListener)
+
+        btnFade.setOnClickListener {
+//            val animator = ValueAnimator.ofFloat(1.0f, 0.0f)
+//            animator.duration = 1000
+//            animator.addUpdateListener { valueAnimator ->
+//                val value = valueAnimator?.animatedValue as Float
+//                tvFade.alpha = value
+//            }
+            val animatorShow = ObjectAnimator.ofFloat(mask, "alpha", 0.0f, 0.2f)
+            val animatorHide = ObjectAnimator.ofFloat(mask, "alpha", 0.2f, 0.0f)
+            val animatorSet = AnimatorSet()
+            animatorSet.duration = 1000
+            animatorSet.playSequentially(animatorShow, animatorHide)
+            animatorSet.start()
+        }
     }
 }
