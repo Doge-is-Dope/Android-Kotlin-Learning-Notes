24d4f1be7e971e07ab9268d82315be600a8653fe
clement.l <maple3312936@outlook.com>
2022-01-11 18:27:50 +0800

    chore: add item layout

--- 21_KotlinFlowSample/app/src/main/java/com/chunchiehliang/kotlinflowsample/data/api/Fact.kt
+++ 21_KotlinFlowSample/app/src/main/java/com/chunchiehliang/kotlinflowsample/data/api/Fact.kt
@@ -3,7 +3,7 @@ package com.chunchiehliang.kotlinflowsample.data.api
 import com.squareup.moshi.Json
 
 data class Fact(
-    @Json(name = "fact") val fact: String,
+    @Json(name = "fact") val content: String,
     @Json(name = "length") val length: Int,
 )
 
