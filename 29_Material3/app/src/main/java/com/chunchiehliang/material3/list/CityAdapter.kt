540c365a0e0cecf9c15e7a6f49600d8b05394180
doge-is-dope <maple3312936@outlook.com>
2022-02-07 23:02:31 +0800

    feat: add list

---
+++ 29_Material3/app/src/main/java/com/chunchiehliang/material3/list/CityAdapter.kt
@@ -0,0 +1,56 @@
+package com.chunchiehliang.material3.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.chunchiehliang.material3.data.City
+import com.chunchiehliang.material3.databinding.ItemCityBinding
+
+class CityAdapter(private val listener: CityListener) :
+    ListAdapter<City, CityAdapter.CityViewHolder>(CityDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
+        return CityViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
+        getItem(position)?.let { user ->
+            holder.bind(user, listener)
+        }
+    }
+
+
+    class CityViewHolder(private var binding: ItemCityBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(city: City, listener: CityListener) {
+            binding.city = city
+            binding.listener = listener
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): CityViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemCityBinding.inflate(layoutInflater, parent, false)
+                return CityViewHolder(binding)
+            }
+        }
+    }
+}
+
+
+class CityDiffCallback : DiffUtil.ItemCallback<City>() {
+    override fun areItemsTheSame(oldItem: City, newItem: City): Boolean {
+        return oldItem.cityId == newItem.cityId
+    }
+
+    override fun areContentsTheSame(oldItem: City, newItem: City): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class CityListener(val listener: (city: City) -> Unit) {
+    fun onCityClick(city: City) = listener(city)
+}
---
+++ 29_Material3/app/src/main/java/com/chunchiehliang/material3/list/ComponentListFragment.kt
@@ -0,0 +1,46 @@
+package com.chunchiehliang.material3.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.PagerSnapHelper
+import com.chunchiehliang.material3.data.City
+import com.chunchiehliang.material3.databinding.FragmentComponentListBinding
+import timber.log.Timber
+
+
+class ComponentListFragment : Fragment() {
+
+    private val cityAdapter by lazy {
+        CityAdapter(listener = CityListener {
+            Timber.d("clicked: $it")
+        })
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentComponentListBinding.inflate(layoutInflater)
+
+        binding.cityList.apply {
+            setHasFixedSize(true)
+            adapter = cityAdapter
+            PagerSnapHelper().attachToRecyclerView(this)
+        }
+
+        cityAdapter.submitList(listOf(City(0, "#EF7C8E", "New York City", "sub", "desc"),
+            City(1, "#FAE8E0", "Boston", "sub", "desc"),
+            City(2, "#B6E2D3", "Philadelphia", "sub", "desc"),
+            City(3, "#D8A7B1", "Washington DC", "sub", "desc"),
+            City(4, "#FFF4BD", "New Haven", "sub", "desc"),
+            City(5, "#F4B9B8", "Cambridge", "sub", "desc"),
+            City(6, "#887BB0", "Providence", "sub", "desc"),
+            City(7, "#85D2D0", "Hartford", "sub", "desc"),
+            City(8, "#376380", "Worcester", "sub", "desc")))
+
+        return binding.root
+    }
+}
