5b72bfb377ef7a3ffaa01971a2846f8754868d63
clement.l <maple3312936@outlook.com>
2022-02-16 18:45:28 +0800

    chore: add lifecyclerowner

--- 30_MemoryLeak/app/src/main/java/com/chunchiehliang/memoryleaksample/FirstFragment.kt
+++ 30_MemoryLeak/app/src/main/java/com/chunchiehliang/memoryleaksample/FirstFragment.kt
@@ -2,24 +2,14 @@ package com.chunchiehliang.memoryleaksample
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.chunchiehliang.memoryleaksample.databinding.FragmentFirstBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class FirstFragment : BaseFragment<FragmentFirstBinding>(FragmentFirstBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Default) {
-            delay(3000)
-            withContext(Dispatchers.Main) {
-                binding.tvLabelFirst.text = "Finished"
-            }
-        }
+        binding.lifecycleOwner = viewLifecycleOwner
 
         binding.btnSecond.setOnClickListener {
             findNavController().navigate(R.id.action_to_second)
