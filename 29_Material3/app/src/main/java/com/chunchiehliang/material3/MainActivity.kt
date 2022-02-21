d406520a780e2a07af9ef524f9b66c07939f62d8
clement.l <maple3312936@outlook.com>
2022-02-21 10:53:13 +0800

    feat: add bottom nav

--- 29_Material3/app/src/main/java/com/chunchiehliang/material3/MainActivity.kt
+++ 29_Material3/app/src/main/java/com/chunchiehliang/material3/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.chunchiehliang.material3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import timber.log.Timber
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Timber.d("main")
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
     }
 }
