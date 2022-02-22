344c6d1d3f4727b2a642d75acf2f27d2ff850014
clement.l <maple3312936@outlook.com>
2022-02-22 14:50:30 +0800

    feat: add filter slider

--- 29_Material3/app/src/main/java/com/chunchiehliang/material3/ui/filter/FilterFragment.kt
+++ 29_Material3/app/src/main/java/com/chunchiehliang/material3/ui/filter/FilterFragment.kt
@@ -1,13 +1,17 @@
 package com.chunchiehliang.material3.ui.filter
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.chunchiehliang.material3.R
 import com.chunchiehliang.material3.databinding.FragmentFilterBinding
 import com.chunchiehliang.material3.utils.JsonUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.slider.RangeSlider
+import com.google.android.material.slider.Slider
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
@@ -35,10 +39,21 @@ class FilterFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.slider.setLabelFormatter { value: Float ->
-            val format = NumberFormat.getCurrencyInstance()
-            format.maximumFractionDigits = 0
-            format.currency = Currency.getInstance("TWD")
-            format.format(value.toInt())
+            formatValue(value)
+        }
+        binding.slider.addOnChangeListener { slider, value, fromUser ->
+            val (minValue, maxValue) = slider.values
+            val formattedMinValueString = formatValue(minValue)
+            val formattedMaxValueString = formatValue(maxValue)
+//            Timber.d("min: $minValue, max: $maxValue")
+            if (minValue == 0.0F && maxValue == 20000.0F) binding.tvPrice.text =
+                getString(R.string.any)
+            else if (maxValue == 20000.0F) binding.tvPrice.text =
+                getString(R.string.above_value, formattedMinValueString)
+            else if (minValue == 0.0F) binding.tvPrice.text =
+                getString(R.string.up_to_value, formattedMaxValueString)
+            else binding.tvPrice.text =
+                getString(R.string.between_values, formattedMinValueString, formattedMaxValueString)
         }
 
         val sizes = args.sizes?.let { JsonUtil.parseStringToIntList(it) }
@@ -54,5 +69,12 @@ class FilterFragment : BottomSheetDialogFragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun formatValue(value: Float): String {
+        val format = NumberFormat.getCurrencyInstance()
+        format.maximumFractionDigits = 0
+        format.currency = Currency.getInstance("TWD")
+        return format.format(value.toInt())
     }
 }
