38cbf13439847345493a41f229b80b22b080ff69
doge-is-dope <maple3312936@outlook.com>
2022-03-08 00:02:25 +0800

    test: add unit test

--- 21_KotlinFlowSample/app/src/test/java/com/chunchiehliang/kotlinflowsample/ExampleUnitTest.kt
+++ 21_KotlinFlowSample/app/src/test/java/com/chunchiehliang/kotlinflowsample/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.chunchiehliang.kotlinflowsample
 
+import org.junit.Assert
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +9,50 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    data class ParentType(
+        val id: Int,
+        val title: String,
+        val show: Boolean,
+        val children: List<ChildType>
+    )
+
+    data class ChildType(val id: Int, val title: String)
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun nested_type() {
+        val data = listOf(
+            ParentType(
+                0, "A", false,
+                listOf(ChildType(0, "aaa"), ChildType(1, "aab"), ChildType(2, "aac"))
+            ),
+            ParentType(
+                1, "B", true,
+                listOf(ChildType(3, "bbb"))
+            ),
+            ParentType(
+                2, "C", true,
+                listOf(ChildType(4, "ccc"), ChildType(5, "cca"), ChildType(6, "ccb"))
+            )
+        )
+
+        val childIdSet = setOf(3, 1, 2)
+
+        val result = mutableListOf<Pair<String, List<String>>>()
+
+        data.forEach { parentType ->
+            val typeList = mutableListOf<String>()
+            parentType.children.forEach { type ->
+                if (type.id in childIdSet) typeList.add(type.title)
+            }
+            if (typeList.isNotEmpty()) result.add(
+                Pair(if (parentType.show) parentType.title else "", typeList)
+            )
+        }
+
+        val sb = StringBuilder()
+        sb.append(result.joinToString(separator = " / ") { if (it.first.isBlank()) it.second.joinToString() else "${it.first} ${it.second.joinToString()}" })
+
+        Assert.assertEquals("aab, aac / B bbb", sb.toString())
     }
 }
