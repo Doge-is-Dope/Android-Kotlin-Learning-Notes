29a703eb2550ef61fbc0449374c071eb549806cb
clement.l <maple3312936@outlook.com>
2022-03-11 11:44:54 +0800

    chore: simplify the condition

--- 30_MemoryLeak/app/src/main/java/com/chunchiehliang/memoryleaksample/MainActivity.kt
+++ 30_MemoryLeak/app/src/main/java/com/chunchiehliang/memoryleaksample/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        findViewById<BottomNavigationView>(R.id.bottom_nav)
-            .setupWithNavController(navController)
+//        findViewById<BottomNavigationView>(R.id.bottom_nav)
+//            .setupWithNavController(navController)
     }
 }
--- 30_MemoryLeak/app/src/main/java/com/chunchiehliang/memoryleaksample/SecondFragment.kt
+++ 30_MemoryLeak/app/src/main/java/com/chunchiehliang/memoryleaksample/SecondFragment.kt
@@ -5,8 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.chunchiehliang.memoryleaksample.databinding.FragmentFirstBinding
 import com.chunchiehliang.memoryleaksample.databinding.FragmentSecondBinding
 
 class SecondFragment :  BaseFragment<FragmentSecondBinding>(FragmentSecondBinding::inflate)  {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_to_first)
+        }
+    }
 }
