2ce58b8882908620b283536f60367ddd5f44e2d2
clement.l <maple3312936@outlook.com>
2022-03-21 11:05:46 +0800

    feat: add uikit

---
+++ SendBirdSample/uikitsample/src/main/java/com/chunchiehliang/uikitsample/BaseApplication.kt
@@ -0,0 +1,12 @@
+package com.chunchiehliang.uikitsample
+
+import android.app.Application
+import timber.log.Timber
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
