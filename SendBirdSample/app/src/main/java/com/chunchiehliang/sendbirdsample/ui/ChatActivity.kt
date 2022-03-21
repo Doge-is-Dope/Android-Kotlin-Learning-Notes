12e854a6f06300e1438b5ca1da2c4be1865902f1
clement.l <maple3312936@outlook.com>
2022-03-21 10:55:35 +0800

    feat: init uikit sample

---
+++ SendBirdSample/app/src/main/java/com/chunchiehliang/sendbirdsample/ui/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.chunchiehliang.sendbirdsample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chunchiehliang.sendbirdsample.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
