1a7d79dfec7daf3902e0663a5556d3a8bb00b40b
clement.l <maple3312936@outlook.com>
2022-03-24 14:12:36 +0800

    feat: implement init and connect

--- SendBirdSample/uikitsample/src/main/java/com/chunchiehliang/uikitsample/MainActivity.kt
+++ SendBirdSample/uikitsample/src/main/java/com/chunchiehliang/uikitsample/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.chunchiehliang.uikitsample
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.chunchiehliang.uikitsample.BuildConfig.SENDBIRD_APP_ID
 import com.chunchiehliang.uikitsample.databinding.ActivityMainBinding
+import com.chunchiehliang.uikitsample.sendbird.SendbirdUtils.connect
+import com.chunchiehliang.uikitsample.sendbird.SendbirdUtils.initSendbird
 import com.chunchiehliang.uikitsample.ui.CustomChannelListActivity
-import com.sendbird.android.SendBirdException
-import com.sendbird.android.handlers.InitResultHandler
-import com.sendbird.uikit.SendBirdUIKit
-import com.sendbird.uikit.adapter.SendBirdUIKitAdapter
-import com.sendbird.uikit.interfaces.UserInfo
-import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,73 +17,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
-            clearFocus()
+            btnInitSendbird.setOnClickListener {
+                initSendbird(applicationContext)
+            }
+
+            btnConnect.setOnClickListener {
+                connect(
+                    userId = "1",
+                    accessToken = "4867e748fb7707ed33c985f2e37d8b25bc22c590",
+                    connectedCallback = { user, isOffline -> showToastMsg("Connected user ${user.nickname}, isOffline: $isOffline") },
+                    connectFailedCallback = { e -> showToastMsg("Connect failed: ${e.message}") }
+                )
+            }
 
             btnToChannelList.setOnClickListener {
-                if (editUserId.text.isNullOrBlank()) {
-                    inputUserId.isErrorEnabled = true
-                    inputUserId.error = "User ID is required"
-                } else {
-                    clearFocus()
-                    initSendbird()
-                    startActivity(Intent(applicationContext, CustomChannelListActivity::class.java))
-                }
+                startActivity(Intent(applicationContext, CustomChannelListActivity::class.java))
             }
 
             btnToChat.setOnClickListener {
-                if (editUserId.text.isNullOrBlank()) {
-                    inputUserId.isErrorEnabled = true
-                    inputUserId.error = "User ID is required"
-                } else {
-                    clearFocus()
-                    initSendbird()
-                }
             }
         }
     }
 
-
-    private fun ActivityMainBinding.clearFocus() {
-        editUserId.clearFocus()
-        root.requestFocus()
-        inputUserId.isErrorEnabled = false
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(editUserId.windowToken, 0)
-    }
-
-    private fun ActivityMainBinding.initSendbird() {
-        SendBirdUIKit.init(object : SendBirdUIKitAdapter {
-            override fun getAppId(): String = SENDBIRD_APP_ID
-
-            override fun getAccessToken(): String = ""
-
-            override fun getUserInfo(): UserInfo {
-                return object : UserInfo {
-                    override fun getUserId(): String = editUserId.text.toString()
-
-                    override fun getNickname(): String = "Cat"
-
-                    override fun getProfileUrl(): String = "https://i.imgur.com/nzM9MUe.png"
-
-                }
-            }
-
-            override fun getInitResultHandler(): InitResultHandler {
-                return object : InitResultHandler {
-                    override fun onInitFailed(e: SendBirdException) {
-                        Timber.e("onInitFailed: $e")
-                    }
-
-                    override fun onInitSucceed() {
-                        Timber.d("onInitSucceed")
-                    }
-
-                    override fun onMigrationStarted() {
-                        Timber.d("onMigrationStarted")
-                    }
-                }
-            }
-
-        }, applicationContext)
-    }
+    private fun showToastMsg(message: String) =
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
--- SendBirdSample/uikitsample/src/main/java/com/chunchiehliang/uikitsample/ui/CustomChannelFragment.kt
+++ SendBirdSample/uikitsample/src/main/java/com/chunchiehliang/uikitsample/ui/CustomChannelFragment.kt
@@ -1,8 +1,27 @@
 package com.chunchiehliang.uikitsample.ui
 
-import com.sendbird.uikit.activities.ChannelActivity
+import com.sendbird.android.UserMessageParams
 import com.sendbird.uikit.fragments.ChannelFragment
+import timber.log.Timber
 
-class CustomChannelFragment: ChannelFragment() {
+class CustomChannelFragment : ChannelFragment() {
 
+    override fun onBeforeSendUserMessage(params: UserMessageParams) {
+        super.onBeforeSendUserMessage(params)
+        val type = getType()
+        Timber.d("type: $type")
+        params.apply {
+            customType = type
+            data = null
+            mentionedUserIds = null
+            metaArrays = null
+            parentMessageId = 0
+        }
+
+
+    }
+
+    private fun getType(): String {
+        return activity.takeIf { it is CustomChannelActivity }.let { "ABC" }
+    }
 }
