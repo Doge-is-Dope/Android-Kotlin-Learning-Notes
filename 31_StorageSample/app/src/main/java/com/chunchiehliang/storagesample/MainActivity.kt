abfeb8eb778e2c01bee856c16855fe721c8d4996
doge-is-dope <maple3312936@outlook.com>
2022-04-13 23:47:31 +0800

    feat: add storage sample

---
+++ 31_StorageSample/app/src/main/java/com/chunchiehliang/storagesample/MainActivity.kt
@@ -0,0 +1,132 @@
+package com.chunchiehliang.storagesample
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+
+
+/** The request code for requesting [Manifest.permission.READ_EXTERNAL_STORAGE] permission. */
+private const val READ_EXTERNAL_STORAGE_REQUEST = 0x1045
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel.images.observe(this) { images ->
+            Log.d("MainActivity", "images: $images")
+        }
+
+        if (!haveStoragePermission()) {
+            Toast.makeText(this, "No permission", Toast.LENGTH_SHORT).show()
+            openMediaStore()
+        } else {
+            showImages()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            READ_EXTERNAL_STORAGE_REQUEST -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PERMISSION_GRANTED) {
+                    showImages()
+                } else {
+                    // If we weren't granted the permission, check to see if we should show
+                    // rationale for the permission.
+                    val showRationale =
+                        ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+
+                    /**
+                     * If we should show the rationale for requesting storage permission, then
+                     * we'll show [ActivityMainBinding.permissionRationaleView] which does this.
+                     *
+                     * If `showRationale` is false, this means the user has not only denied
+                     * the permission, but they've clicked "Don't ask again". In this case
+                     * we send the user to the settings page for the app so they can grant
+                     * the permission (Yay!) or uninstall the app.
+                     */
+                    if (showRationale) {
+                        showNoAccess()
+                    } else {
+                        goToSettings()
+                    }
+                }
+                return
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+
+    private fun openMediaStore() {
+        if (haveStoragePermission()) {
+            showImages()
+        } else {
+            requestPermission()
+        }
+    }
+
+
+    /**
+     * Convenience method to check if [Manifest.permission.READ_EXTERNAL_STORAGE] permission
+     * has been granted to the app.
+     */
+    private fun haveStoragePermission() =
+        ContextCompat.checkSelfPermission(
+            this, Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PERMISSION_GRANTED
+
+
+    /**
+     * Convenience method to request [Manifest.permission.READ_EXTERNAL_STORAGE] permission.
+     */
+    private fun requestPermission() {
+        if (!haveStoragePermission()) {
+            val permissions = arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            ActivityCompat.requestPermissions(this, permissions, READ_EXTERNAL_STORAGE_REQUEST)
+        }
+    }
+
+    private fun showImages() {
+        viewModel.loadImages()
+    }
+
+    private fun showNoAccess() {
+//        binding.welcomeView.visibility = View.GONE
+//        binding.permissionRationaleView.visibility = View.VISIBLE
+    }
+
+    private fun goToSettings() {
+        Intent(ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:$packageName")).apply {
+            addCategory(Intent.CATEGORY_DEFAULT)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }.also { intent ->
+            startActivity(intent)
+        }
+    }
+}
